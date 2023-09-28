fa2f2ba962434bc3c2e273136b17a94e2feae4f9
PushkarGaur10 <gaurpushkar10@gmail.com>
2023-09-28 17:26:54 +0530

    283

--- pages/Community/index.js
+++ pages/Community/index.js
@@ -1,133 +1,155 @@
-import React from 'react'
-import Topcomponent from '../../components/homepage/Topcomponent'
-import Footer from '../../components/homepage/bottomcomponent/footer'
-import styles from '../../styles/homecomponents/community.module.css'
-import Image from 'next/image'
+import React from "react";
+import Topcomponent from "../../components/homepage/topcomponent";
+import Footer from "../../components/homepage/bottomcomponent/footer";
+import styles from "../../styles/homecomponents/community.module.css";
+import Image from "next/image";
 
 const index = () => {
-
   const arr = [
-    { url : '/booksimages/1 (1).jpeg',
-      name : 'Dan Brown',
-      genre : 'Thriller',
-      id:1,
-                },
-    { url : '/booksimages/1 (2).jpeg',
-      name : 'JK Rowling',
-      genre : 'Magical Realism',
-      id:2,
-                },
-    { url : '/booksimages/1 (3).jpeg',
-      name : 'Arundhati Roy',
-      genre : 'Historical Fiction',
-      id:3,
-                },
-    { url : '/booksimages/1 (4).jpeg',
-      name : 'Ahmed Salman Rushdie',
-      genre : 'Magical Realism',
-      id:4,
-                },
-    { url : '/booksimages/1 (5).jpeg',
-      name : 'Nilanjana Sudeshna Jhumpa Lahiri',
-      genre : 'Historical Fiction',
-      id:5,
-                },
-    { url : '/booksimages/1 (6).jpeg',
-      name : 'Jeffrey Archer',
-      genre : 'Thriller',
-      id:6,
-                },
-    { url : '/booksimages/1 (7).jpeg',
-      name : 'Charles Dickens',
-      genre : 'Classic',
-      id:7,
-                },
-    { url : '/booksimages/1 (8).jpeg',
-      name : 'Ernest Hemingway',
-      genre : 'Classic',
-      id:8,
-                },    
-    { url : '/booksimages/1 (9).jpeg',
-      name : 'Jane Austen',
-      genre : 'Classic',
-      id:9,
-                },  
-]
+    {
+      url: "/booksimages/1 (1).jpeg",
+      name: "Dan Brown",
+      genre: "Thriller",
+      id: 1,
+    },
+    {
+      url: "/booksimages/1 (2).jpeg",
+      name: "JK Rowling",
+      genre: "Magical Realism",
+      id: 2,
+    },
+    {
+      url: "/booksimages/1 (3).jpeg",
+      name: "Arundhati Roy",
+      genre: "Historical Fiction",
+      id: 3,
+    },
+    {
+      url: "/booksimages/1 (4).jpeg",
+      name: "Ahmed Salman Rushdie",
+      genre: "Magical Realism",
+      id: 4,
+    },
+    {
+      url: "/booksimages/1 (5).jpeg",
+      name: "Nilanjana Sudeshna Jhumpa Lahiri",
+      genre: "Historical Fiction",
+      id: 5,
+    },
+    {
+      url: "/booksimages/1 (6).jpeg",
+      name: "Jeffrey Archer",
+      genre: "Thriller",
+      id: 6,
+    },
+    {
+      url: "/booksimages/1 (7).jpeg",
+      name: "Charles Dickens",
+      genre: "Classic",
+      id: 7,
+    },
+    {
+      url: "/booksimages/1 (8).jpeg",
+      name: "Ernest Hemingway",
+      genre: "Classic",
+      id: 8,
+    },
+    {
+      url: "/booksimages/1 (9).jpeg",
+      name: "Jane Austen",
+      genre: "Classic",
+      id: 9,
+    },
+  ];
   const arr2 = [
-  { url : '/booksimages/2 (1).jpeg',
-    name : 'Carl Sagan',
-    genre : 'Popular Science',
-    id:1,
-              },
-  { url : '/booksimages/2 (2).jpeg',
-    name : 'Yuval Noah Harari',
-    genre : 'Historical Non-Fiction',
-    id:2,
-              },
-  { url : '/booksimages/2 (3).jpeg',
-    name : 'Richard Feynhan',
-    genre : 'Popular Science',
-    id:3,
-              },
-  { url : '/booksimages/2 (4).jpeg',
-    name : 'Richard Dawkins',
-    genre : 'Popular Science',
-    id:4,
-              },
-  { url : '/booksimages/2 (5).jpeg',
-    name : 'Siddharth Mukherjee',
-    genre : 'Genral Non-Fiction',
-    id:5,
-              },
-]
+    {
+      url: "/booksimages/2 (1).jpeg",
+      name: "Carl Sagan",
+      genre: "Popular Science",
+      id: 1,
+    },
+    {
+      url: "/booksimages/2 (2).jpeg",
+      name: "Yuval Noah Harari",
+      genre: "Historical Non-Fiction",
+      id: 2,
+    },
+    {
+      url: "/booksimages/2 (3).jpeg",
+      name: "Richard Feynhan",
+      genre: "Popular Science",
+      id: 3,
+    },
+    {
+      url: "/booksimages/2 (4).jpeg",
+      name: "Richard Dawkins",
+      genre: "Popular Science",
+      id: 4,
+    },
+    {
+      url: "/booksimages/2 (5).jpeg",
+      name: "Siddharth Mukherjee",
+      genre: "Genral Non-Fiction",
+      id: 5,
+    },
+  ];
 
   return (
     <div>
       <Topcomponent />
       <div className={styles.container}>
-    <div className={styles.topcontainer}>
-      <div className={styles.topic}>Join a community</div>
-      <hr className={styles.hr}/>
-    </div>
-
-      <div className={styles.bvsection}>
-
-    <h1 className={styles.title}>Fiction</h1>
-  
-  <div className={styles.bvcontainer}>
-  {arr.map(data =>
-      <div className={styles.bvcard} key={data.id}>
-        <div className={styles.bvimgcont}>
-          <Image src={data.url} width={400} height={400} alt="Book Image" />
+        <div className={styles.topcontainer}>
+          <div className={styles.topic}>Join a community</div>
+          <hr className={styles.hr} />
         </div>
-        <div className={styles.bvcardinfo}>
-          <h2>{data.name}</h2>
-          <h4>Genre - {data.genre}</h4>
+
+        <div className={styles.bvsection}>
+          <h1 className={styles.title}>Fiction</h1>
+
+          <div className={styles.bvcontainer}>
+            {arr.map((data) => (
+              <div className={styles.bvcard} key={data.id}>
+                <div className={styles.bvimgcont}>
+                  <Image
+                    src={data.url}
+                    width={400}
+                    height={400}
+                    alt="Book Image"
+                  />
+                </div>
+                <div className={styles.bvcardinfo}>
+                  <h2>{data.name}</h2>
+                  <h4>Genre - {data.genre}</h4>
+                </div>
+              </div>
+            ))}
+          </div>
+
+          <h1 className={styles.title}>Non-Fiction</h1>
+
+          <div className={styles.bvcontainer}>
+            {arr2.map((data) => (
+              <div className={styles.bvcard} key={data.id}>
+                <div className={styles.bvimgcont}>
+                  <Image
+                    src={data.url}
+                    width={400}
+                    height={400}
+                    alt="Book Image"
+                  />
+                </div>
+                <div className={styles.bvcardinfo}>
+                  <h2>{data.name}</h2>
+                  <h4>Genre - {data.genre}</h4>
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
-      </div>)}
       </div>
-
-  
-      <h1 className={styles.title}>Non-Fiction</h1>
-  
-      <div className={styles.bvcontainer}>
-      {arr2.map(data =>
-      <div className={styles.bvcard} key={data.id}>
-        <div className={styles.bvimgcont}>
-          <Image src={data.url} width={400} height={400} alt="Book Image" />
-        </div>
-        <div className={styles.bvcardinfo}>
-          <h2>{data.name}</h2>
-          <h4>Genre - {data.genre}</h4>
-        </div>
-      </div>)}
-      </div>
-    
-  </div>
-  </div>
       <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default index
+export default index;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,13 @@
-import Head from 'next/head'
-import { Inter } from 'next/font/google'
-import Topcomponent from '../components/homepage/Topcomponent'
-import Herosection from '../components/homepage/herosection'
-import Reading from '../components/homepage/middlecomponents/reading'
-import Books from '../components/homepage/middlecomponents/bookssection'
-import Lower from '../components/homepage/lowercomponents/lowercomponents'
-import Footer from '../components/homepage/bottomcomponent/footer'
+import Head from "next/head";
+import { Inter } from "next/font/google";
+import Topcomponent from "../components/homepage/topcomponent";
+import Herosection from "../components/homepage/herosection";
+import Reading from "../components/homepage/middlecomponents/reading";
+import Books from "../components/homepage/middlecomponents/bookssection";
+import Lower from "../components/homepage/lowercomponents/lowercomponents";
+import Footer from "../components/homepage/bottomcomponent/footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
@@ -27,5 +27,5 @@ export default function Home() {
         <Footer />
       </div>
     </>
-  )
+  );
 }
