ecb58ee201836827c9a9122145b82d05d654acd5
PushkarGaur10 <gaurpushkar10@gmail.com>
2023-10-05 18:13:22 +0530

    55

--- pages/Book/[id].js
+++ pages/Book/[id].js
@@ -5,7 +5,7 @@ import styles from '../../styles/singlebook/book.module.css'
 import Image from 'next/image';
 
 export const getStaticPaths = async () => {
-  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/books`, {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_URL}/books?pagination[page]=1&pagination[pageSize]=100`, {
     headers: {
      Authorization: "bearer "+process.env.NEXT_PUBLIC_TOKEN,
    } 
@@ -41,8 +41,6 @@ export const getStaticProps = async (context) => {
 };
 
 const Book = ({product}) => {
-
-  console.log(product)
 
   const [show, setShow] = useState(true)
 
