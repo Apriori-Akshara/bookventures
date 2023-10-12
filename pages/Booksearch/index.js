685861941a2c49cc1e743d4d2d077bcb66e9bfcf
PushkarGaur10 <gaurpushkar10@gmail.com>
2023-10-12 14:29:30 +0530

    121

--- pages/Booksearch/index.js
+++ pages/Booksearch/index.js
@@ -1,26 +1,35 @@
-import React from 'react'
+import React, { useState,useEffect } from 'react'
 import Topcomponent from "../../components/homepage/topcomponent";
 import Footer from "../../components/homepage/bottomcomponent/footer";
 import styles from '../../styles/booksearch/booksearch.module.css'
 import Link from 'next/link';
 import Image from 'next/image';
-import {BsSearch} from 'react-icons/bs'
+// import {BsSearch} from 'react-icons/bs'
+import axios from "axios";
 
-export const getStaticProps = async () => {
-  const resulting = await fetch(`${process.env.NEXT_PUBLIC_URL}/books?populate=*`, {
-    headers: {
-     Authorization: "bearer "+process.env.NEXT_PUBLIC_TOKEN,
-   } 
-   });
-  const result = await resulting.json();
-  return {
-    props: {
-      data: result.data,
-    },
-  };
+const index = () => {
+
+  const[product,setProduct] = useState([]);
+  const[starts,setStarts] =useState('')
+
+  useEffect(() => {
+    const fetchData = async() => {
+  try{
+    const res = await axios.get(process.env.NEXT_PUBLIC_URL+`/books?filters[name][$startsWithi]=${starts}&populate=*`, {
+     headers: {
+      Authorization: "bearer "+process.env.NEXT_PUBLIC_TOKEN,
+    } 
+    });
+    setProduct(res.data.data);
+    console.log(res.data.data)
+  } catch(err) {
+    console.log(err);
+  }
 };
+fetchData();
+}, [starts]);
 
-const index = ({data}) => {
+console.log(starts)
   
   return (
     <div>
@@ -29,11 +38,11 @@ const index = ({data}) => {
       <div className={styles.topic}>Search For The Books You Like</div>
       <h1 className={styles.title}>Search</h1>
       <div className={styles.searchcontainer}>
-        <input placeholder='search by writing the name' />
-        <button><BsSearch /></button>
+        <input placeholder='search by writing the name' value={starts} onChange={e => setStarts(e.target.value)} />
+        {/* <button><BsSearch /></button> */}
       </div>
       <div className={styles.cards}>
-        {data.reverse().slice(0,8).map(data=>{
+        {product.reverse().slice(0,8).map(data=>{
           return <div className={styles.card} key={data.id}>
           <Link className={styles.link} href={`/Book/${data.id}`}>
             <Image className={styles.img} loader={() => data.attributes.img.data.attributes.url} unoptimized={true} src={data.attributes.img.data.attributes.url} width={400} height={400} alt="Book Image" />
