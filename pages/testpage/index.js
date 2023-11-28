4e591e3a687db19926759fb65e2f6834f6f703f7
PushkarGaur10 <gaurpushkar10@gmail.com>
2023-11-28 12:15:46 +0530

    283

--- pages/testpage/index.js
+++ pages/testpage/index.js
@@ -1,6 +1,6 @@
 import Navbar from '../../components/testpage/navbar/navbar' 
 import Hero from '../../components/testpage/herosection/hero'
-import Slider from '../../components/testpage/herosection/Heroslider'
+import Slider from '../../components/testpage/herosection/Heroslider.js'
 
 const index = () => {
 
