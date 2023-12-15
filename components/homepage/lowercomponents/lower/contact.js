44164ff8da2b5b596d8c1a6623a9fe4838cbc3b1
PushkarGaur10 <gaurpushkar10@gmail.com>
2023-12-15 16:45:52 +0530

    157

--- components/homepage/lowercomponents/lower/contact.js
+++ components/homepage/lowercomponents/lower/contact.js
@@ -13,7 +13,7 @@ const contact = () => {
         <div className={styles.left}>
           <h4>Message us on WhatsApp</h4>
           {/* <p>We love our book club members, so feel free to visit during normal business hours.</p> */}
-          <button>WhatsApp<BsWhatsapp /></button>
+          <a className={styles.linked} href="https://wa.me/9717189138?text=welcome" title="Share on whatsapp"><button>WhatsApp<BsWhatsapp /></button></a>
           <h4>Bookventures</h4>
           <p>Delhi, India</p>
           <p>Open today 09:00 am - 05:00 pm</p>
